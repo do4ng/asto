5dc7f684c0e53fd966ed3151188c6c6ad7100d77
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-09-22 00:11:34 +0900

    feat: asset loader replace

--- packages/asto/src/loader/asset.ts
+++ packages/asto/src/loader/asset.ts
@@ -1,23 +1,30 @@
-import fs from 'node:fs';
+import fs, { readFileSync, writeFileSync } from 'node:fs';
 import path from 'node:path';
 
 import { Loader } from '$asto/types/loader';
 
-const copy = (src: string, dest: string) => {
+const copy = (src: string, dest: string, replace: Record<string, string>) => {
   const exists = fs.existsSync(src);
   const stats = exists && fs.statSync(src);
   const isDirectory = exists && stats.isDirectory();
   if (isDirectory) {
     fs.mkdirSync(dest);
     fs.readdirSync(src).forEach((childItemName) => {
-      copy(path.join(src, childItemName), path.join(dest, childItemName));
+      copy(path.join(src, childItemName), path.join(dest, childItemName), replace);
     });
   } else {
+    let target = readFileSync(src).toString();
+
+    Object.keys(replace).forEach((r) => {
+      target = target.replace(r, replace[r]);
+    });
+
+    writeFileSync(dest, target);
     fs.copyFileSync(src, dest);
   }
 };
 export const assetLoader = () =>
-  <Loader<never>>{
+  <Loader<any>>{
     name: 'asset',
     builders: {
       asset: (ctx) => {
@@ -28,7 +35,7 @@ export const assetLoader = () =>
           };
         }
 
-        copy(ctx.input, ctx.output);
+        copy(ctx.input, ctx.output, ctx.options?.replace || []);
 
         return {
           success: true,
